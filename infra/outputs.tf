baafb61e19c75d0663a1677e0d9ee66d1e0e371e
hamsa <hamsahahmed7@gmail.com>
2025-11-17 18:22:40 +0000

    update: updated terrafrom modules also iam module added

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,3 +3,8 @@ output "domain_name" {
   description = "Application domain name"
   value       = var.domain_name
 }
+
+output "https_domain_name" {
+  description = "Deployment environment name"
+  value       = "https://${var.domain_name}"
+}
