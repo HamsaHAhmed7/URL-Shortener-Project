06a34f526c1ebf11983b82a1e90908701de6ea26
hamsa <hamsahahmed7@gmail.com>
2025-11-17 18:58:49 +0000

    testing: new terraform pipeline

---
+++ infra/modules/github-oidc/main.tf
@@ -0,0 +1,40 @@
+# Get the OIDC provider (created manually in AWS Console)
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+# IAM Role for GitHub Actions
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-deploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = data.aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+        }
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+
+  tags = {
+    Name        = "GitHub Actions Deploy Role"
+    ManagedBy   = "Terraform"
+    Environment = "production"
+  }
+}
+
+# Attach AdministratorAccess (for portfolio - in production use least privilege)
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
